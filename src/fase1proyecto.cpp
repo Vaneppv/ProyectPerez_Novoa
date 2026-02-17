38d95017e05597397f414e26605e2806424e5fa8
Vanessa Perez <vaneppv@gmail.com>
2026-02-17 17:51:34 -0400

    feat: Validaciones y busqueda

--- src/fase1proyecto.cpp
+++ src/fase1proyecto.cpp
@@ -98,12 +98,17 @@ void inicializarTienda(Tienda* tienda, const char* nombre, const char* rif){
     strncpy(tienda->rif, rif, 19);
     tienda->rif[19] = '\0';
     
+    // Establecer capacidades iniciales
+    tienda->capacidadClientes = 5;
+    tienda->capacidadProductos = 5;
+    tienda->capacidadProveedores = 5;
+    tienda->capacidadTransacciones = 5;
 
     // Reservar capacidad inicial en los arreglos
-    tienda->proveedores = new Proveedor [5];
-    tienda->clientes = new Cliente [5];
-    tienda->productos = new Producto [5];
-    tienda->transacciones = new Transaccion [5];
+    tienda->proveedores = new Proveedor [tienda->capacidadProveedores];
+    tienda->clientes = new Cliente [tienda->capacidadClientes];
+    tienda->productos = new Producto [tienda->capacidadProductos];
+    tienda->transacciones = new Transaccion [tienda->capacidadTransacciones];
 
     // Inicializar contadores de cantidad (stock) en 0
     tienda->numProductos = 0;
@@ -116,12 +121,6 @@ void inicializarTienda(Tienda* tienda, const char* nombre, const char* rif){
     tienda->siguienteIdCliente = 1;
     tienda->siguienteIdProducto = 1;
     tienda->siguienteIdProveedor = 1;
-
-    // Establecer capacidades iniciales
-    tienda->capacidadClientes = 5;
-    tienda->capacidadProductos = 5;
-    tienda->capacidadProveedores = 5;
-    tienda->capacidadTransacciones = 5;
 
 }
 
@@ -265,7 +264,11 @@ void eliminarCliente(Tienda* tienda){
 
 // Registrar Compra (a Proveedor)
 void registrarCompra(Tienda* tienda){
-
+    if (tienda == nullptr){ // Validacion de seguridad
+        return;
+    }
+    
+    
 }
 
 
@@ -297,130 +300,340 @@ void cancelarTransaccion(Tienda* tienda){
 
 // Redimensionamiento de Arrays
 void redimensionarProductos(Tienda* tienda){
-    int nuevaCapacidad = tienda->capacidadProductos * 2;
-    Producto* nuevoArray = new Producto[nuevaCapacidad];
+    int nuevaCapacidad = tienda->capacidadProductos * 2; // Aumento de la capacidad del arreglo al doble
+    Producto* nuevoArray = new Producto[nuevaCapacidad]; // Creacion de un nuevo arreglo con la nueva capacidad
 
-    for (int i = 0; i < tienda->numProductos; i++)
+    for (int i = 0; i < tienda->numProductos; i++) // Copia de datos al nuevo arreglo
     {
         nuevoArray[i] = tienda->productos[i];
     }
     
-    delete[] tienda->productos;
-    tienda->capacidadProductos = nuevaCapacidad;
-    tienda->productos = nuevoArray;
+    delete[] tienda->productos; // Liberacion del arreglo viejo
+    tienda->capacidadProductos = nuevaCapacidad; // Actualizacion de capacidad
+    tienda->productos = nuevoArray; // Actualizacion del punero al nuevo array
 }
 
 
 void redimensionarProveedores(Tienda* tienda){
-    int nuevaCapacidad = tienda->capacidadProveedores * 2;
-    Proveedor* nuevoArray = new Proveedor[nuevaCapacidad];
+    int nuevaCapacidad = tienda->capacidadProveedores * 2; // Aumento de la capacidad del arreglo al doble
+    Proveedor* nuevoArray = new Proveedor[nuevaCapacidad]; // Creacion de un nuevo arreglo con la nueva capacidad
 
-    for (int i = 0; i < tienda->numProveedores; i++)
+    for (int i = 0; i < tienda->numProveedores; i++) // Copia de datos al nuevo arreglo
     {
         nuevoArray[i] = tienda->proveedores[i];
     }
     
-    delete[] tienda->proveedores;
-    tienda->capacidadProveedores = nuevaCapacidad;
-    tienda->proveedores = nuevoArray;
+    delete[] tienda->proveedores; // Liberacion del arreglo viejo
+    tienda->capacidadProveedores = nuevaCapacidad; // Actualizacion de capacidad
+    tienda->proveedores = nuevoArray; // Actualizacion del punero al nuevo array
 }
 
 
 void redimensionarClientes(Tienda* tienda){
-    int nuevaCapacidad = tienda->capacidadClientes * 2;
-    Cliente* nuevoArray = new Cliente[nuevaCapacidad];
+    int nuevaCapacidad = tienda->capacidadClientes * 2; // Aumento de la capacidad del arreglo al doble
+    Cliente* nuevoArray = new Cliente[nuevaCapacidad]; // Creacion de un nuevo arreglo con la nueva capacidad
 
-    for (int i = 0; i < tienda->numClientes; i++)
+    for (int i = 0; i < tienda->numClientes; i++) // Copia de datos al nuevo arreglo
     {
         nuevoArray[i] = tienda->clientes[i];
     }
     
-    delete[] tienda->clientes;
-    tienda->capacidadClientes = nuevaCapacidad;
-    tienda->clientes = nuevoArray;
+    delete[] tienda->clientes; // Liberacion del arreglo viejo
+    tienda->capacidadClientes = nuevaCapacidad; // Actualizacion de capacidad
+    tienda->clientes = nuevoArray; // Actualizacion del punero al nuevo array
 }
 
 
 void redimensionarTransacciones(Tienda* tienda){
-    int nuevaCapacidad = tienda->capacidadTransacciones * 2;
-    Transaccion* nuevoArray = new Transaccion[nuevaCapacidad];
+    int nuevaCapacidad = tienda->capacidadTransacciones * 2;// Aumento de la capacidad del arreglo al doble
+    Transaccion* nuevoArray = new Transaccion[nuevaCapacidad]; // Creacion de un nuevo arreglo con la nueva capacidad
 
-    for (int i = 0; i < tienda->numTransacciones; i++)
+    for (int i = 0; i < tienda->numTransacciones; i++)  // Copia de datos al nuevo arreglo
     {
         nuevoArray[i] = tienda->transacciones[i];
     }
     
-    delete[] tienda->transacciones;
-    tienda->capacidadTransacciones = nuevaCapacidad;
-    tienda->transacciones = nuevoArray;
+    delete[] tienda->transacciones; // Liberacion del arreglo viejo
+    tienda->capacidadTransacciones = nuevaCapacidad; // Actualizacion de capacidad
+    tienda->transacciones = nuevoArray; // Actualizacion del punero al nuevo array
 }
 
 
 // VALIDACIONES
 bool validarEmail(const char* email){
+    const char* at = nullptr; 
+    const char* dot = nullptr;
 
+    // Recorrido de la cadena en busca del '@'
+    for (int i = 0; email[i] != '\0' ; i++) 
+    {
+        if (email[i] == '@') 
+        {
+            // Una vez localizado se le asigna la direccion de memoria
+            at = &email[i];
+            // Recorrido de la cadena en busca del '.' 
+            for (int j = i + 1; email[j] != '\0'; j++)
+            {
+                if (email[j] == '.')
+                {
+                    // Una vez localizado se le asigna la direccion de memoria
+                    dot = &email[j];
+                    break;
+                }
+            }
+        break;   
+        }
+    }
+    return (at != nullptr && dot != nullptr); // Retorna True si ambos son diferentes a Nullptr
 }
 
 
-bool validarFecha(const char* fecha){
+bool validarFecha(const char* fecha) {
 
+    if (strlen(fecha) != 10) return false; // Validar de tamaño de caracteres (YYYY-MM-DD)
+    if (fecha[4] != '-' || fecha[7] != '-') return false; // Validar de caracteres en posicion 4 y 5 (deben ser '-')
+
+    // Validar que sean numeros excepto los '-'
+    for (int i = 0; i < 10; i++) {
+        if (i == 4 || i == 7) continue;
+        if (fecha[i] < '0' || fecha[i] > '9') return false;
+    }
+
+    // Conversion de texto char a int 
+    int anio = (fecha[0]-'0')*1000 + (fecha[1]-'0')*100 + (fecha[2]-'0')*10 + (fecha[3]-'0');
+    int mes  = (fecha[5]-'0')*10 + (fecha[6]-'0');
+    int dia  = (fecha[8]-'0')*10 + (fecha[9]-'0');
+
+    // Validad que la fecha este en los rangos logicos
+    if (anio < 1900 || anio > 2100) return false; 
+    if (mes < 1 || mes > 12) return false;
+    if (dia < 1 || dia > 31) return false;
+
+    // Validar dias para meses de 30 dias y Feb
+    if ((mes == 4 || mes == 6 || mes == 9 || mes == 11) && dia > 30) return false;
+    if (mes == 2) 
+    {
+        if (dia > 29) return false;
+    }
+    return true; // Fecha valida
 }
 
 
 bool existeProducto(Tienda* tienda, int id){
 
+    if (tienda == nullptr){ // Validacion de seguridad
+    return false;
+    } 
+    // Recorrido del arreglo en busca del id
+    for (int i = 0; i < tienda->numProductos; i++) 
+    {
+        if (tienda->productos[i].id == id) 
+        {
+            return true; // Si lo encuenta retorna True (Si existe el producto)
+        }
+    }
+    return false; // No existe
 }
 
 
 bool existeProveedor(Tienda* tienda, int id){
-
+    if (tienda == nullptr){ // Validacion de seguridad
+    return false;
+    } 
+    // Recorrido del arreglo en busca del id
+    for (int i = 0; i < tienda->numProveedores; i++)
+    {
+        if (tienda->proveedores[i].id == id)
+        {
+            return true; // Si lo encuenta retorna True (Si existe el proveedor)
+        }
+    }
+    return false; // No existe
 }
 
 
 bool existeCliente(Tienda* tienda, int id){
-
+    if (tienda == nullptr){ // Validacion de seguridad
+        return false;
+    }
+    // Recorrido del arreglo en busca del id
+    for (int i = 0; i < tienda->numClientes; i++)
+    {
+        if (tienda->clientes[i].id == id)
+        {
+            return true; // Si lo encuenta retorna True (Si existe el cliente)
+        }
+    }
+    return false; // No existe
 }
 
 
 bool codigoDuplicado(Tienda* tienda, const char* codigo){
-
+    if (tienda == nullptr){ // Validacion de seguridad
+        return false;
+    }
+    // Recorrido del arreglo en busca del codigo
+    for (int i = 0; i < tienda->numProductos; i++)
+    {
+        if (strcmp(tienda->productos[i].codigo, codigo) == 0)
+        {
+            return true; // Si lo encuenta retorna True (Si existe el codigo)
+        }
+    }
+    return false; // No esta duplicado
 }
 
 
 bool rifDuplicado(Tienda* tienda, const char* rif){
-
+    if (tienda == nullptr){ // Validacion de seguridad
+        return false;
+    }
+    // Recorrido del arreglo en busca del rif
+    for (int i = 0; i < tienda->numProveedores; i++)
+    {
+        if (strcmp(tienda->proveedores[i].rif, rif) == 0)
+        {
+            return true; // Si lo encuenta retorna True (Si existe el rif)
+        }
+    }
+    // Recorrido del arreglo en busca de la cedula
+    for (int j = 0; j < tienda->numClientes; j++)
+    {
+        if (strcmp(tienda->clientes[j].cedula, rif) == 0)
+        {
+            return true; // Si lo encuenta retorna True (Si existe la cedula)
+        }
+    }
+    return false; // No esta duplicado
 }
 
 
 // BUSQUEDAS 
 
 int buscarProductoPorId(Tienda* tienda, int id){
-
+    if (tienda == nullptr){ 
+        return -1;
+    }
+    // Recorrido del arreglo en busca del producto
+    for (int i = 0; i < tienda->numProductos; i++)
+    {
+        if (tienda->productos[i].id == id)
+        {
+            return i; // Si lo encuentra retorna el indice
+        }
+    }
+    return -1; // No existe
 }
 
 
 int buscarProveedorPorId(Tienda* tienda, int id){
-
+    if (tienda == nullptr){ // Validacion de seguridad
+        return -1;
+    }
+    // Recorrido del arreglo en busca del proveedor
+    for (int i = 0; i < tienda->numProveedores; i++)
+    {
+        if (tienda->proveedores[i].id == id)
+        {
+            return i; // Si lo encuentra retorna el indice
+        }
+    }
+    return -1; // No existe
 }
 
 
 int buscarClientePorId(Tienda* tienda, int id){
+    if (tienda == nullptr){ // Validacion de seguridad
+        return -1;
+    }
+    // Recorrido del arreglo en busca del cliente    
+    for (int i = 0; i < tienda->numClientes; i++)
+    {
+        if (tienda->clientes[i].id == id)
+        {
+            return i; // Si lo encuentra retorna el indice
+        }
+    }
+    return -1; // No existe
+}
 
+int* buscarProductosPorNombre(Tienda* tienda, const char* nombre, int* numResultados){
+    if (tienda == nullptr){ // Validacion de seguridad
+        return nullptr;
+    }
+    // Inicializacion del contador de resultados encontrados
+    *numResultados = 0;
+    // Recorrido para saber tamaño del arreglo de productos encontrados
+    for (int i = 0; i < tienda->numProductos; i++) // Verificar si hay coincidencias
+    {
+        if (contieneSubstring(tienda->productos[i].nombre, nombre))
+        {
+            (*numResultados)++; 
+        }
+    }
+    if (*numResultados == 0){ // Si no hay coincidencias retorna nullptr
+        return nullptr;
+    }
+    // Reservar memoria para arreglo de productos encontrados
+    int* indiceProductos = new int[*numResultados];
+    int posicion = 0;
+    // Llenar arreglo con indices reales de los productos encontrados
+    for (int i = 0; i < tienda->numProductos; i++) // Verificar si hay coincidencias
+    {
+        if (contieneSubstring(tienda->productos[i].nombre, nombre))
+        {
+            indiceProductos[posicion] = i;
+            posicion++;
+        }
+    }
+    return indiceProductos; // Retorna el puntero al arreglo de indices
 }
 
 // UTILIDADES
 void obtenerFechaActual(char* buffer){
+    time_t tiempoActual = time (0); // Obtener tiempo actual del sistema
+    
+    tm* fechaActual = localtime(&tiempoActual); // Conversion a estructura local
 
+    strftime(buffer, 11, "%Y-%m-%d", fechaActual); // Organizar la informacion al formato YYYY-MM-DD
 }
 
 
 void convertirAMinusculas(char* cadena){
-
+    if (cadena == nullptr){ // Validacion de Seguridad
+        return;
+    }
+    // Conversion de caracteres a letras minusculas 
+    for (int i = 0; cadena[i] != 0; i++)
+    {
+        cadena[i] = tolower((unsigned char)cadena[i]);
+    }
 }
 
 
 bool contieneSubstring(const char* texto, const char* busqueda){
+    if (texto == nullptr || busqueda == nullptr){ // Validacion de Seguridad
+        return false;
+    }
+    // Creacion de variables para evitar modificar las originales
+    char textoP[100];
+    char busquedaP[100];
+    
+    // Copiar y convertir a minusculas  
+    strncpy(textoP, texto, 99);
+    textoP[99] = '\0';
+    convertirAMinusculas(textoP);
 
+    strncpy(busquedaP, busqueda, 99);
+    busquedaP[99] = '\0';
+    convertirAMinusculas(busquedaP);
+
+    // Busqueda de coincidencias
+    if (strstr(textoP, busquedaP) != nullptr){
+        return true;
+    }
+
+    return false; // Retorna False si no hubo coincidencias
 }
 
 
